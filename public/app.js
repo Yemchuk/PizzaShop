e396261dc74520c22a5cadf8738efc07b1667777
Oleh <emchuk86@gmail.com>
2019-01-31 18:42:15 +0200

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@
 function something()
 {
 	var x = window.localStorage.getItem('bbb');
-	x = x * 1 + 1;
+	x = x * 1 + 1; // x = x + 1
 	window.localStorage.setItem('bbb', x);
 
 	alert(x);
@@ -10,5 +10,9 @@ function something()
 
 function add_to_cart(id)
 {
-	alert('You added pizza with id: '+ id);
+	var key = 'product_' + id;
+	
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
 }
