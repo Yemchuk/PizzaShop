a82da36f26047830c476e3b9faf9292115e428df
Oleh <emchuk86@gmail.com>
2019-02-06 16:53:07 +0200

    add model Order and migration

---
+++ db/migrate/20190206144941_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+	def change
+		create_table :orders do |t|
+		t.text :name
+		t.text :phone
+		t.text :address
+
+		t.timestamps
+  		end
+	end
+end
